be5f95e9f89875e30435cf33f427c049c753b900
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-10 19:16:10 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "PASCAL Context"
 PROJECT_NAME_FULL: str = "PASCAL Context Dataset"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -97,6 +98,7 @@ def get_settings():
 
     settings = {
         "project_name": PROJECT_NAME,
+        "hide_dataset": HIDE_DATASET,
         "license": LICENSE,
         "applications": APPLICATIONS,
         "category": CATEGORY,
